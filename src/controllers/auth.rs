82bf3f979f156e8489ac9c21dffd3d236c2b5b6f
Imre Halasz <halimi@freemail.hu>
2025-09-30 23:37:43 +0200

    Implement Sign Up

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,8 +1,36 @@
-use actix_web::{post, Responder};
+use actix_web::{HttpResponse, Responder, post, web};
+use serde::Deserialize;
+use serde_json::json;
 
+use crate::{
+    AppState,
+    db::{self, user},
+};
+
+#[derive(Deserialize, Debug)]
+pub struct SignUpRequest {
+    pub email: String,
+    pub password: String,
+    pub firstname: String,
+    pub lastname: String,
+}
 #[post("/auth/sign-up")]
-pub async fn sign_up() -> impl Responder {
-    "Sign Up"
+pub async fn sign_up(state: web::Data<AppState>, data: web::Json<SignUpRequest>) -> impl Responder {
+    let db = state.db.lock().await;
+
+    if db::user::has_with_email(&db, &data.email).await {
+        return HttpResponse::UnprocessableEntity().json(json!({
+            "status": "error",
+            "message": "Email already exists."
+        }));
+    }
+
+    db::user::create(&db, &data).await;
+
+    HttpResponse::Created().json(json!({
+        "status": "success",
+        "message": "Account created successfully."
+    }))
 }
 
 #[post("/auth/sign-in")]
---
+++ src/db/user.rs
@@ -0,0 +1,26 @@
+use bcrypt::{DEFAULT_COST, hash};
+
+use crate::controllers::auth::SignUpRequest;
+
+pub async fn has_with_email(db: &sqlx::MySqlPool, email: &str) -> bool {
+    sqlx::query!("SELECT * FROM users WHERE email = ?", email)
+        .fetch_optional(db)
+        .await
+        .unwrap()
+        .is_some()
+}
+
+pub async fn create(db: &sqlx::MySqlPool, user: &SignUpRequest) -> bool {
+    let hashed_password = hash(&user.password, DEFAULT_COST).unwrap();
+
+    sqlx::query!(
+        "INSERT INTO users (`email`, `password`, `firstname`, `lastname`) VALUES (?, ?, ?, ?)",
+        &user.email,
+        &hashed_password,
+        &user.firstname,
+        &user.lastname
+    )
+    .execute(db)
+    .await
+    .is_ok()
+}
