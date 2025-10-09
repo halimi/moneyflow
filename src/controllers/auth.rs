f7d2676082d763fe9d3c9c4df00364989c6bf8c4
Imre Halasz <halimi@freemail.hu>
2025-10-09 23:33:31 +0200

    Implement Sign In

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,5 +1,8 @@
+use std::time::SystemTime;
+
 use actix_web::{HttpResponse, Responder, post, web};
-use serde::Deserialize;
+use jsonwebtoken::{EncodingKey, Header};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::{
@@ -14,6 +17,7 @@ pub struct SignUpRequest {
     pub firstname: String,
     pub lastname: String,
 }
+
 #[post("/auth/sign-up")]
 pub async fn sign_up(state: web::Data<AppState>, data: web::Json<SignUpRequest>) -> impl Responder {
     let db = state.db.lock().await;
@@ -33,7 +37,55 @@ pub async fn sign_up(state: web::Data<AppState>, data: web::Json<SignUpRequest>)
     }))
 }
 
+#[derive(Deserialize, Debug)]
+pub struct SignInRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Claims {
+    pub sub: u64,
+    pub role: String,
+    pub exp: u64,
+}
+
 #[post("/auth/sign-in")]
-pub async fn sign_in() -> impl Responder {
-    "Sign In"
+pub async fn sign_in(state: web::Data<AppState>, data: web::Json<SignInRequest>) -> impl Responder {
+    let db = state.db.lock().await;
+    let Some(user) = db::user::get_by_email(&db, &data.email).await else {
+        return HttpResponse::Unauthorized().json(json!({
+            "status": "error",
+            "message": "Invalid email or password."
+        }));
+    };
+
+    if !bcrypt::verify(&data.password, &user.password).unwrap() {
+        return HttpResponse::Unauthorized().json(json!({
+            "status": "error",
+            "message": "Invalid email or password."
+        }));
+    }
+
+    let claims = Claims {
+        sub: user.id,
+        role: "user".to_string(),
+        exp: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            + 4 * 60 * 60,
+    };
+
+    let token = jsonwebtoken::encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(state.jwt_secret.as_bytes()),
+    )
+    .unwrap();
+
+    HttpResponse::Ok().json(json!({
+        "status": "success",
+        "token": token
+    }))
 }
