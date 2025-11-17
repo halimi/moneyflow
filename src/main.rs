33db2f02e9e3754540ae730b6b345e4bdc236e2f
Imre Halasz <halimi@freemail.hu>
2025-11-17 22:58:51 +0100

    Add CORS

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use actix_cors::Cors;
 use actix_extensible_rate_limit::{
     RateLimiter,
     backend::{SimpleInputFunctionBuilder, memory::InMemoryBackend},
@@ -34,6 +35,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Cors::permissive())
             .wrap(
                 RateLimiter::builder(
                     rate_limiter_backend.clone(),
